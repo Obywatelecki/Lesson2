ccf30b9ad794c9b664ce20923baa48d7d3d60e55
Obywatelecki <tpanecki@gmail.com>
2017-12-16 13:16:18 +0100

    Initial commit

---
+++ src/Programme.java
@@ -0,0 +1,29 @@
+import com.mypackage.MyClass;
+
+public class Programme {
+
+    public static void inc(int x) {
+        x++;
+    }
+
+    public static void inc1(Integer x) {
+        x++;
+    }
+
+    public static void main(String[] args) {
+
+        Integer x = 7;
+        int y = 7;
+        inc(x);
+        inc1(y);
+
+        System.out.println(x);
+        System.out.println(y);
+
+        MyClass obj = new MyClass(1, 2);
+        obj.name = "Tomek";
+        System.out.println(obj.sum());
+
+    }
+}
+
---
+++ src/com/mypackage/MyClass.java
@@ -0,0 +1,17 @@
+package com.mypackage;
+
+public class MyClass {
+
+    public Integer x;
+    public Integer y;
+    public String name;
+
+    public MyClass(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Integer sum() {
+        return x + y;
+    }
+}
