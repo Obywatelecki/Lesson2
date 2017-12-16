bf3c7f033e936fde60b17691a1e39c5fbfdc7117
Obywatelecki <tpanecki@gmail.com>
2017-12-16 15:10:20 +0100

    wywalone dwa atrybuty

--- src/com/App.java
+++ src/com/App.java
@@ -1,5 +1,4 @@
 package com;
-
 import com.Coord.Coord;
 import com.Coord.Coord3;
 
@@ -10,6 +9,7 @@ public class App {
 
         Coord c1 = new Coord(1, 2);
         Coord3 c2 = new Coord3(23, 42, 44);
+
         System.out.println("Diameter: " + c1.diameter());
         System.out.println("Sum c1: " + c1.sum());
         System.out.println("Sum c2: " + c2.sum());
