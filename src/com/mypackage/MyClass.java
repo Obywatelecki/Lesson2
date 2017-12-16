90ef381a2fb15b0cc9f72f40104bae690a7444a0
Obywatelecki <tpanecki@gmail.com>
2017-12-16 13:24:34 +0100

    Testowy komentarz

--- src/com/mypackage/MyClass.java
+++ src/com/mypackage/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
     public Integer x;
     public Integer y;
     public String name;
+    public Boolean czyTuli;
 
     public MyClass(Integer x, Integer y) {
         this.x = x;
