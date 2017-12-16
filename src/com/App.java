967fefd8418762e770251ca35c9f2f0415bc74d8
Obywatelecki <tpanecki@gmail.com>
2017-12-16 16:37:29 +0100

    refactor

--- src/com/App.java
+++ src/com/App.java
@@ -1,17 +1,36 @@
 package com;
+
 import com.Coord.Coord;
 import com.Coord.Coord3;
 
 public class App {
+
+    public static void increment(int[] x, int idx) {
+        x[idx]++;
+    }
 
     public static void main(String[] args) {
 
         Coord c1 = new Coord(1, 2);
         Coord3 c2 = new Coord3(23, 42, 44);
 
-        System.out.println("Diameter: " + c1.diameter());
+        System.out.println("Diameter: " + c1.diameter() + "\n");
+        System.out.println("Sum c1: " + c1.sum() + "\n");
+        System.out.println("Sum c2: " + c2.sum() + "\n");
+
+        Coord.greet();
+
+        c1.setX(10);
+        c2.setY(20);
+
         System.out.println("Sum c1: " + c1.sum());
-        System.out.println("Sum c2: " + c2.sum());
+
+        int[] x = {3, 6, 5};
+        System.out.println(x[0]);
+        increment(x, 0);
+        System.out.println(x[0]);
+
+        System.out.println(c1.multisum(5));
     }
 }
 
--- src/com/Coord/Coord.java
+++ src/com/Coord/Coord.java
@@ -4,10 +4,19 @@ public class Coord {
 
     protected double x;
     protected double y;
+    protected final double statVal = 10.0; // cos jak const w c++
 
     public Coord(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static void greet() {
+        System.out.println("Lubie zurek!");
+    }
+
+    public void setX(double x) {
+        this.x = x;
     }
 
     public double diameter() {
@@ -16,5 +25,14 @@ public class Coord {
 
     public double sum() {
         return x + y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double multisum(double multi) {
+        double value = sum() * multi;
+        return value;
     }
 }
